574299cb1838141630821941a256b968baf3a013
gnana03 <46742808+gnana03@users.noreply.github.com>
2019-11-07 12:12:23 +0530

    Update secrets_client.py

--- api/secrets_client.py
+++ api/secrets_client.py
@@ -4,19 +4,9 @@ from kubernetes import client
 
 class SecretsClient:
 
-    def get_pods_for_all_namespaces(self):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
+    def get_secret_for_all_namespaces(self):
+        return client.CoreV1Api().list_secret_for_all_namespaces(watch=False)
 
-    def get_pods_for_all_namespaces_with_http_info(self):
+    def get_secret_for_all_namespaces_with_http_info(self):
         BaseClass().load_config()
-        return client.CoreV1Api().list_pod_for_all_namespaces_with_http_info(watch=False)
-
-    def get_pods_for_namespace(self, namepace):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_namespaced_pod(namepace, watch=False)
-
-    def get_namespaced_pods_with_http_info(self, namespace):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_namespaced_pod_with_http_info(namespace, watch=False)
-
+        return client.CoreV1Api().list_secret_for_all_namespaces_with_http_info(watch=False)
