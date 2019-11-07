92c05c21f91e410344b0eb3d27340d473fcecfea
gnana03 <46742808+gnana03@users.noreply.github.com>
2019-11-07 12:15:13 +0530

    Update daemon_client.py

--- api/daemon_client.py
+++ api/daemon_client.py
@@ -5,9 +5,7 @@ from kubernetes import client
 class DaemonClient:
 
     def get_daemons_for_all_namespaces(self):
-        BaseClass().load_config()
         return client.CoreV1Api().list_daemon_set_for_all_namespaceswatch=False)
 
     def get_pods_for_all_namespaces_with_http_info(self):
-        BaseClass().load_config()
         return client.CoreV1Api().list_daemon_set_for_all_namespaces_with_http_info(watch=False)
