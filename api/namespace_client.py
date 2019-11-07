08ab99dadcb1235d14cf4e3dd49d5e3887d98c19
gnana03 <46742808+gnana03@users.noreply.github.com>
2019-11-07 12:08:30 +0530

    Update namespace_client.py

--- api/namespace_client.py
+++ api/namespace_client.py
@@ -4,19 +4,18 @@ from kubernetes import client
 
 class NameSpaceClient:
 
-    def get_pods_for_all_namespaces(self):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_pod_for_all_namespaces(watch=False)
+    def get_namespace(self):
+        return client.CoreV1Api().list_namespace(watch=False)
 
-    def get_pods_for_all_namespaces_with_http_info(self):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_pod_for_all_namespaces_with_http_info(watch=False)
-
-    def get_pods_for_namespace(self, namepace):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_namespaced_pod(namepace, watch=False)
-
-    def get_namespaced_pods_with_http_info(self, namespace):
-        BaseClass().load_config()
-        return client.CoreV1Api().list_namespaced_pod_with_http_info(namespace, watch=False)
-
+    def get_namespaces_with_http_info(self):
+        return client.CoreV1Api().list_namespace_with_http_info(watch=False)
+    
+    def get_namespaced_config_map(self,namespace):
+        return client.CoreV1Api().list_namespaced_config_map(watch=False,namespace)
+    
+    def get_namespaced_pod(self,namespace):
+        return client.CoreV1Api().list_namespaced_pod(watch=False,namespace)
+    
+    def get_namespaced_rc(self,namespace):
+        return client.CoreV1Api().list_namespaced_replication_controller(watch=False,namespace)
+       
