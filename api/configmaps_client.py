adb1ea997e8763afb5629944d1a2989b6a95b900
gnana03 <46742808+gnana03@users.noreply.github.com>
2019-11-07 12:02:12 +0530

    Update configmaps_client.py

--- api/configmaps_client.py
+++ api/configmaps_client.py
@@ -7,13 +7,13 @@ class ConfigMaps_client:
     v1 = client.CoreV1Api()
 
     def __get__config_maps_for_all_namespaces__(self):
-        return v1.list_config_map_for_all_namespaces
+        return v1.list_config_map_for_all_namespaces(watch=False)
 
     def __get__config_maps_for_all_namespaces_with_http_info__(self, namepace):
-        return v1.list_config_map_for_all_namespaces_with_http_info()
+        return v1.list_config_map_for_all_namespaces_with_http_info(watch=False)
 
     def __get__config_maps_for_namespace__(self, namepace):
-        return v1.list_namespaced_config_map(namepace)
+        return v1.list_namespaced_config_map(namepace,watch=False)
 
     def _get___namespaced_config_map_with_http_info__(self, namespace):
-        return v1.list_namespaced_config_map_with_http_info(namespace)
+        return v1.list_namespaced_config_map_with_http_info(namespace,watch=False)
