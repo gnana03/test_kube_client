65c00d835183efd48c9b4ff901d5a0c89a328b2f
gnana03 <46742808+gnana03@users.noreply.github.com>
2019-11-13 12:50:54 +0530

    Update test_deployment_crud.py

--- tests/test_deployment_crud.py
+++ tests/test_deployment_crud.py
@@ -12,20 +12,6 @@ class TestCases(DeploymentFixtures):
 
    # @pytest.mark.parametrize('crud_deployment',"nginx-deployment", indirect=True)
     def test_deployment_crud(self,crud_deployment):
-        # Configs can be set in Configuration class directly or using helper
-        # utility. If no argument provided, the config will be loaded from
-        # default location.
-#       config.load_kube_config()
-#       apps_v1 = client.AppsV1Api()
-        # Create a deployment object with client-python API. The deployment we
-        # created is same as the `nginx-deployment.yaml` in the /examples folder.
-#        deployment = simple_deployment
-#
-#        create_deployment deployment)
-
-#        update_deployment(apps_v1, deployment)
-
-        #delete_deployment("nginx-deployment")
         print("test starts now")
         DEPLOYMENT_NAME = "nginx-deployment"
         crud_deployment(self,DEPLOYMENT_NAME)
